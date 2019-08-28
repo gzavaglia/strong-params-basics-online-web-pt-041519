12d64dfd53a5466bfffe24696edb7287fa03d859
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-28 19:38:21 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,9 @@ class PostsController < ApplicationController
   @post.update(post_params(:title))
   redirect_to post_path(@post)
 	end
+	def edit
+	  @post = Post.find(params[:id])
+	end
 	
 	private
 	
@@ -29,7 +32,5 @@ class PostsController < ApplicationController
   params.require(:post).permit(*args)
 end
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
+	
 end
